fed3c65e2a7dc7c6ef7e05baa7092c82ef48ea78
rannyeribaptist <rannyerirbatista@gmail.com>
2021-02-06 19:25:53 -0300

    refactored code

--- highlighter.rb
+++ highlighter.rb
@@ -1,23 +1,75 @@
 require 'securerandom'
+
+content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas consectetur malesuada velit, sit amet porta magna maximus nec. Aliquam aliquet tincidunt enim vel rutrum. Ut augue lorem, rutrum et turpis in, molestie mollis nisi. Ut dapibus erat eget felis pulvinar, ac vestibulum augue bibendum. Quisque sagittis magna nisi. Sed aliquam porttitor fermentum. Nulla consequat justo eu nulla sollicitudin auctor. Sed porta enim non diam mollis, a ullamcorper dolor molestie. Nam eu ex non nisl viverra hendrerit. Donec ante augue, eleifend vel eleifend quis, laoreet volutpat ipsum. Integer viverra aliquam nulla, ac rutrum dui sodales nec.
+
+Sed turpis enim, porttitor nec maximus sed, luctus pretium elit. Sed sodales imperdiet velit, vitae viverra erat commodo non. Nunc porttitor risus sit amet quam faucibus, et luctus ex fringilla. Mauris quis urna non lacus tempor iaculis vitae quis dolor. Nam vitae pulvinar lacus, quis varius erat. Etiam lobortis orci vitae elementum tempor. Praesent convallis euismod enim vel vestibulum. Proin vitae eros vitae nisi cursus dapibus vitae at ipsum. Phasellus sed tempor eros, non scelerisque nunc. Nullam condimentum ex ultrices, ultrices ante sit amet, rhoncus nibh. Aliquam fermentum vulputate fringilla. Ut risus orci, pharetra eu tellus vel, fringilla feugiat dolor.
+
+Nunc quis elit quam. Sed aliquet, nibh ut sagittis egestas, lorem tortor laoreet diam, non maximus lectus dolor dignissim eros. Sed vehicula mi id aliquet aliquam. Vestibulum sed lacus et neque dictum convallis in vitae mauris. Etiam varius augue vel mattis tempor. Curabitur mattis facilisis metus, tempus consectetur quam aliquam sed. Mauris velit orci, efficitur sit amet nisl in, finibus dictum elit. In lectus augue, elementum eu sapien sed, auctor tincidunt urna.
+
+Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer lacinia accumsan velit. Duis vel facilisis libero. Cras consequat sit amet mauris ut ultrices. Ut pulvinar sit amet odio sit amet pretium. Nullam tortor ligula, consequat non nisl vitae, rutrum placerat est. Sed finibus interdum justo vel placerat. Cras varius tortor sed justo tempus scelerisque. Praesent facilisis ex vitae iaculis iaculis. Sed consectetur a lectus non condimentum. Etiam id lacus a nulla cursus laoreet. Vivamus ipsum purus, sodales vel metus varius, viverra mollis justo. Nulla facilisi. Vivamus volutpat nunc elit, quis sollicitudin velit ornare sit amet.
+
+Nullam fringilla nisi nunc, vitae accumsan tortor luctus quis. Sed facilisis, est ut eleifend sagittis, felis dolor pellentesque lectus, in congue purus orci non nunc. Nunc finibus eu metus et volutpat. Integer hendrerit tortor et tellus euismod vulputate. Aliquam erat volutpat. Aenean gravida justo in risus feugiat, ut suscipit tortor ullamcorper. Nam a sapien dictum, vestibulum eros vitae, sodales turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed ultricies at elit et rutrum. Sed placerat erat quis condimentum convallis. Duis ornare magna nec ante faucibus malesuada. Duis a erat sed sapien semper eleifend. Mauris consequat nibh sollicitudin mi euismod, non ultricies lectus bibendum. Cras a erat libero. Aliquam nisl ipsum, scelerisque at risus a, hendrerit vestibulum sapien. Proin luctus diam eu mi lobortis molestie id vel ante."
+
+highlights = [{
+  start_word: 20,
+  end_word: 35,
+  comment: 'Foo'
+}, {
+  start_word: 73,
+  end_word: 92,
+  comment: 'Bar'
+}, {
+  start_word: 50,
+  end_word: 98,
+  comment: 'Baz'
+}]
+
 class TextHighlight
   def initialize(text, highlights)
-    @html = '<p>' + text.gsub("\n\n", '</p><p>') + '</p>'
+    @html = set_html(text)
     @words = @html.split(' ')
     @highlights = highlights
   end
 
   def apply
-    @highlights.each do |highlight|
+    @highlights.each_with_index do |highlight, i|
       start_word = highlight[:start_word]
       end_word = highlight[:end_word]
       content = highlight[:comment]
-      color = SecureRandom.hex(3)
+      colors = create_uniq_colors
 
-      @words[start_word] = '<div class="tooltip_box" style="color: #' + color + '}"><span>' + content + '</span>' + @words[start_word]
-      @words[end_word] = @words[end_word] + '</div>'
+      @words[start_word] = open_highlight(content, colors[i], @words[start_word])
+      @words[end_word] = close_highlight(@words[end_word])
     end
 
     return @words.join(' ')
   end
+
+  private
+
+    def set_html(text)
+      return '<p>' + text.gsub("\n\n", '</p><p>') + '</p>'
+    end
+
+    def open_highlight(text, color, word)
+      return '<div class="tooltip_box" style="color: #' + color + '"><span>' + text + '</span>' + word
+    end
+
+    def close_highlight(word)
+      return word + '</div>'
+    end
+
+    def create_uniq_colors()
+      colors = []
+      @highlights.count.times{ colors.push(SecureRandom.hex(3)) }
+      repeated = colors.detect{ |e| colors.count(e) > 1 }
+
+      if repeated
+        create_uniq_colors
+      else
+        return colors
+      end
+    end
 end
 
+puts TextHighlight.new(content, highlights).apply
